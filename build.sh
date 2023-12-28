7a3b2a0f59d016250ddfef65e14e3d9314cf3129
tu4wkl <pachadomenic2010@gmail.com>
2023-12-28 15:32:56 +0200

    remove windows build command

--- build.sh
+++ build.sh
@@ -11,26 +11,5 @@ if [ -z "$1" ]; then
     exit
 fi
 
-# Validate XML
-#./validatexml.sh
-#if [[ $? -ne 0 ]]; then
-#    echo "XML validation failed. Exiting..."
-#    exit 1
-#fi
-
-# Pull overlays
-for device in "${supported_devices[@]}"; do
-    mkdir -p device/${device}/overlay/
-done
-
 rm -f $1
-lowercase_arg="${1,,}"
-if [[ "$lowercase_arg" == *"patch"* ]]; then
-    rm device/*/boot.img
-    rm mods/magisk.zip
-    zip -v -r $1 META-INF/com META-INF/scripts/bin mods img device csc debloat featherproject.keys auxy/data/app/.set #META-INF/scripts/xbin/mounts.kek.br
-    adb push $lowercase_arg /sdcard/
-    adb shell twrp install /sdcard/$lowercase_arg
-else
-    D:\oneuidev\TukiAroma\zip\bin\zip.exe -v -r $1 META-INF/com META-INF/scripts/bin mods img device auxy csc debloat featherproject.keys META-INF/scripts/xbin
-fi
+zip -v -r $1 META-INF/com META-INF/scripts/bin mods img device auxy csc debloat featherproject.keys META-INF/scripts/xbin
