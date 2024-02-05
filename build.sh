4ad08f5f061e77803415d3b71eddd61a7ce23d27
Pachadomenic <pachadomenic2010@gmail.com>
2024-02-05 18:51:21 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@
 # To be used when ones images are ready
 # Call with the format ./build.sh FeatherProject_S22_T_v0.7.zip
 
-supported_devices=("m31" "m21" "m31s" "a51" "f41")
 
 if [ -z "$1" ]; then
     echo "Filename not provided"
@@ -12,4 +11,4 @@ if [ -z "$1" ]; then
 fi
 
 rm -f $1
-zip -v -r $1 META-INF/com META-INF/scripts/bin mods rom device auxy csc debloat featherproject.keys META-INF/scripts/xbin
+zip -v -r $1 META-INF/com META-INF/scripts/bin mods rom device auxy debloat
